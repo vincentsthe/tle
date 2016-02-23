d315855898ac121671fe22171f76958be8c722cf
Vincent The <vincent.s.the@gmail.com>
2016-02-23 12:38:33 +0700

    refactor code

--- migrator/problemMigrator.js
+++ migrator/problemMigrator.js
@@ -10,33 +10,20 @@ problemMigrator.migrate = function (limit, callback) {
   async.waterfall([
     function (callback) {
       lastIdService.getKeyLastId(lastIdService.PROBLEMSET_PROBLEM_LAST_ID_KEY, function (err, lastId) {
-        if (err) {
-          callback("error get last " + lastIdService.PROBLEMSET_PROBLEM_LAST_ID_KEY + " id: " + err);
-        } else {
-          callback(null, lastId);
-        }
+        callback(err, lastId);
       });
     }, function (problemsetLastId, callback) {
       tlxProblemService.fetchProblemFromJerahmeelProblemset(problemsetLastId, limit, function (err, problems, problemsetMaxId) {
-        if (err) {
-          callback("error fetching problem records from jerahmeel: " + err);
-        } else {
-          callback(null, problems, problemsetMaxId);
-        }
+        callback(err, problems, problemsetMaxId);
       });
     }, function (problems, problemsetMaxId, callback) {
       lastIdService.getKeyLastId(lastIdService.COURSE_PROBLEM_LAST_ID_KEY, function (err, courseLastId) {
-        if (err) {
-          callback("error get last " + lastIdService.COURSE_PROBLEM_LAST_ID_KEY + " id: " + err);
-        } else {
-          callback(null, problems, problemsetMaxId, courseLastId);
-        }
+        callback(err, problems, problemsetMaxId, courseLastId);
       });
     }, function (problems, problemsetMaxId, courseLastId, callback) {
       tlxProblemService.fetchProblemFromJerahmeelCourse(courseLastId, limit - problems.length, function (err, courseProblems, courseMaxId) {
         if (err) {
           console.log("error fetching problem from course: " + err);
-          callback(null, problems, problemsetMaxId, 0);
         } else {
           problems = problems.concat(courseProblems);
           callback(null, problems, problemsetMaxId, courseMaxId);
@@ -44,39 +31,23 @@ problemMigrator.migrate = function (limit, callback) {
       });
     }, function (problems, problemsetMaxId, courseMaxId, callback) {
       tlxProblemService.fillProblemSlugFromSandalphon(problems, function (err, problemRecords) {
-        if (err) {
-          callback("error fetching problem record from sandalphon: " + err);
-        } else {
-          callback(null, problemRecords, problemsetMaxId, courseMaxId);
-        }
+        callback(err, problemRecords, problemsetMaxId, courseMaxId);
       });
     }, function (problems, problemsetMaxId, courseMaxId, callback) {
       if (problems.length) {
         problemService.insertProblem(problems, function (err) {
-          if (err) {
-            callback("error inserting to db: " + err);
-          } else {
-            callback(null, problems.length, problemsetMaxId, courseMaxId);
-          }
+          callback(err, problems.length, problemsetMaxId, courseMaxId);
         });
       } else {
         callback(null, 0, 0, 0);
       }
     }, function (problemCount, problemsetMaxId, courseMaxId, callback) {
       lastIdService.updateLastId(lastIdService.PROBLEMSET_PROBLEM_LAST_ID_KEY, problemsetMaxId, function (err) {
-        if (err) {
-          callback("error updating last id for " + lastIdService.PROBLEMSET_PROBLEM_LAST_ID_KEY + ": " + err);
-        } else {
-          callback(null, problemCount, courseMaxId);
-        }
+        callback(err, problemCount, courseMaxId);
       });
     }, function (problemCount, courseMaxId, callback) {
       lastIdService.updateLastId(lastIdService.COURSE_PROBLEM_LAST_ID_KEY, courseMaxId, function (err) {
-        if (err) {
-          callback("error updating last id for " + lastIdService.COURSE_PROBLEM_LAST_ID_KEY + ": " + err);
-        } else {
-          callback(null, problemCount);
-        }
+        callback(err, problemCount);
       });
     }
   ], function (err, problemCount) {
--- migrator/userMigrator.js
+++ migrator/userMigrator.js
@@ -9,35 +9,19 @@ userMigrator.migrate = function (limit, callback) {
   async.waterfall([
     function (callback) {
       userService.getLastJophielUserId(function (err, lastId) {
-        if (err) {
-          callback("error getting last id from db: " + err);
-        } else {
-          callback(null, lastId);
-        }
+        callback(err, lastId);
       });
     }, function (lastId, callback) {
       tlxUserService.fetchUserFromJophielFromLastId(lastId, limit, function (err, users) {
-        if (err) {
-          callback("error retrieving user from jophiel: " + err);
-        } else {
-          callback(null, users);
-        }
+        callback(err, users);
       });
     }, function (userRecords, callback) {
       userService.insertUser(userRecords, function (err, result) {
-        if (err) {
-          callback("error inserting user: " + err);
-        } else {
-          callback(null, result);
-        }
+        callback(err, result);
       });
     }
   ], function (err, result) {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, result);
-    }
+    callback(err, result);
   });
 };
 
